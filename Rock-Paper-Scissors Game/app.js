ad348bdf5e161f9a0f27af5fdf6b5983d8bbcdde
Ali <alidrl26@gmail.com>
2023-06-12 20:30:59 +0200

    project complated

--- Rock-Paper-Scissors Game/app.js
+++ Rock-Paper-Scissors Game/app.js
@@ -1,147 +1,245 @@
-//* ------ Selectors ------- */
-const selectionArticle = document.querySelector(".selection")
+/* Selectors */
+const selectionArticle = document.querySelector(".selection");
+const yourChoiceDiv = document.getElementById("your-choice");
+const pcChoiceDiv = document.getElementById("pc-choice");
+const messagePar = document.querySelector(".message");
+const scoreCard = document.querySelector(".score-card");
+const pcScoreSpan = document.querySelector("#pc-score");
+const yourScoreSpan = document.querySelector("#your-score");
+const modalCardSection = document.querySelector(".modal-card");
+const finalMessagePar = document.querySelector("#final-message");
+const playAgainBtn = document.querySelector("#play-again");
 
-//? Secilen elemanlarin tayicilari
-const yourChoiceDiv = document.getElementById("your-choice")
-const pcChoiceDiv = document.getElementById("pc-choice")
+/* Variables */
+const userSelectimage = document.createElement("img");
+const pcSelectimage = document.createElement("img");
 
-//? message
-const messagePar = document.querySelector(".message")
+const YELLOW = "#ffc538";
+const RED = "#fb778b";
+const GREEN = "#5ab7ac";
 
-//? Score
-const scoreCardSection = document.querySelector(".score-card")
-const pcScoreSpan = document.getElementById("pc-score")
-const yourScoreSpan = document.getElementById("your-score")
-
-//? Modal
-const modalCardSection = document.querySelector(".modal-card")
-const finalMessagePar = document.getElementById("final-message")
-const playAgainBtn = document.getElementById("play-again")
-
-//* ------- Variables ------- */
-let userSelectImg = document.createElement("img")
-let pcSelectImg = document.createElement("img")
-let pcRandom
-//? Colors
-const YELLOW = "#ffc538"
-const RED = "#fb778b"
-const GREEN = "#5ab7ac"
-
-//* ------- Event Listeners ------- */
+/* Event Listeners */
 selectionArticle.addEventListener("click", (e) => {
-  // console.log(e.target.id)
   if (e.target.id) {
-    userSelectImg.src = `./assets/${e.target.id}.png`
-    userSelectImg.alt = e.target.id
-    yourChoiceDiv.appendChild(userSelectImg)
-    createPcSelection()
+    userSelectimage.src = e.target.src;
+    userSelectimage.alt = e.target.id;
+    yourChoiceDiv.append(userSelectimage);
+    createPcSelection();
   }
-})
+});
 
-playAgainBtn.addEventListener("click", () => {
-  // modalCardSection.classList.toggle("show")
-  // modalCardSection.classList.toggle("remove")
-  modalCardSection.style.display = "none"
+
+playAgainBtn.addEventListener('click',()=>{
+  modalCardSection.classList.toggle('show')
   window.location.reload()
 })
 
-//* ------- Functions ------- */
-
+/* Functions */
 const createPcSelection = () => {
-  const pcArr = ["rock", "paper", "scissor"]
-  pcRandom = pcArr[Math.floor(Math.random() * 3)]
-  pcSelectImg.src = `./assets/${pcRandom}.png`
-  pcSelectImg.alt = pcRandom
-  pcChoiceDiv.appendChild(pcSelectImg)
-  calculateResult()
-}
+  const pcArr = ["rock", "paper", "scissor"];
+  const pcRandom = pcArr[Math.floor(Math.random() * 3)];
+  pcSelectimage.src = `./assets/${pcRandom}.png`;
+  pcSelectimage.alt = pcRandom;
+  pcChoiceDiv.append(pcSelectimage);
+  calculateResult();
+};
 
 const calculateResult = () => {
-  // console.log(userSelectImg.alt)
-  // console.log(pcSelectImg.alt)
-
-  //? Esitlik durumu
-  if (userSelectImg.alt === pcRandom) {
-    draw()
-  } else {
-    if (userSelectImg.alt === "rock") {
-      pcRandom === "paper" ? youLost() : youWin()
-    } else if (userSelectImg.alt === "scissor") {
-      pcRandom === "rock" ? youLost() : youWin()
-    } else if (userSelectImg.alt === "paper") {
-      pcRandom === "scissor" ? youLost() : youWin()
-    }
+  if (userSelectimage.alt === pcSelectimage.alt) {
+    draw();
+  } else if (userSelectimage.alt === "rock") {
+    pcSelectimage.alt === "paper" ? youLost() : youWin();
+  } else if (userSelectimage.alt === "scissor") {
+    pcSelectimage.alt === "rock" ? youLost() : youWin();
+  } else if (userSelectimage.alt === "paper") {
+    pcSelectimage.alt === "scissor" ? youLost() : youWin();
   }
 
-  if (pcScoreSpan.textContent === "10" || yourScoreSpan.textContent === "10") {
-    openModal()
+  document.querySelector('#top-score').textContent = `${yourScoreSpan.textContent} : ${ pcScoreSpan.textContent}`
+  if (pcScoreSpan.textContent === "5" || yourScoreSpan.textContent === "5") {
+    openModal();
   }
-}
+};
 
 const draw = () => {
-  messagePar.textContent = "Its a draw"
-  scoreCardSection.style.color = YELLOW
-  messagePar.style.backgroundColor = YELLOW
-}
+  messagePar.textContent = "It is row";
+  scoreCard.style.color = YELLOW;
+  messagePar.style.backgroundColor = YELLOW;
+};
 
 const youLost = () => {
-  messagePar.textContent = "You Lost"
-  scoreCardSection.style.color = RED
-  messagePar.style.backgroundColor = RED
-  pcScoreSpan.textContent++
-}
-
+  messagePar.textContent = "You lost";
+  scoreCard.style.color = RED;
+  messagePar.style.backgroundColor = RED;
+  pcScoreSpan.textContent++;
+};
 const youWin = () => {
-  messagePar.textContent = "You Win"
-  scoreCardSection.style.color = GREEN
-  messagePar.style.backgroundColor = GREEN
-  yourScoreSpan.textContent++
-}
+  messagePar.textContent = "You Won";
+  scoreCard.style.color = GREEN;
+  messagePar.style.backgroundColor = GREEN;
+  yourScoreSpan.textContent++;
+};
 
-//? modal aç
 const openModal = () => {
-  modalCardSection.classList.add("show")
-
-  if (yourScoreSpan.textContent === "10") {
-    //? eger kullanici 10 puana usalti ise kullanici kazanmistir.
-    finalMessagePar.textContent = "💃 You Win🕺"
-    document.querySelector(".modal").style.backgroundColor = GREEN
-    playAgainBtn.style.color = GREEN
+  modalCardSection.classList.add("show");
+  if (yourScoreSpan.textContent === "5") {
+    finalMessagePar.textContent = "😎 You Won 😎";
+    document.querySelector(".modal").style.backgroundColor = GREEN;
+    playAgainBtn.style.color = GREEN;
+    messagePar.style.display = 'none'
   } else {
-    //? eger pc 10 puana ulasti ise pc kazanmistir.
-    finalMessagePar.textContent = "☹️ You Lost ☹️"
-    document.querySelector(".modal").style.backgroundColor = RED
-    playAgainBtn.style.color = RED
+    finalMessagePar.textContent = "😂 You Lost 😂";
+    document.querySelector(".modal").style.backgroundColor = RED;
+    playAgainBtn.style.color = RED;
+    messagePar.style.display = 'none'
+
   }
-}
+};
 
-//! Local Storage'a veri yazma ve okuma
-localStorage.setItem("highScore", 5) //? veri yazma
-console.log(localStorage.getItem("highScore")) //? veri okuma
+// //* ------ Selectors ------- */
+// const selectionArticle = document.querySelector(".selection");
 
-//! İlkel Yontem
-//? Resimler
-// const rockImg = document.getElementById("rock")
-// const paperImg = document.getElementById("paper")
-// const scissorImg = document.getElementById("scissor")
+// //? Secilen elemanlarin tayicilari
+// const yourChoiceDiv = document.getElementById("your-choice");
+// const pcChoiceDiv = document.getElementById("pc-choice");
 
-// rockImg.addEventListener("click", () => {
-//   image.src = "./assets/rock.png"
-//   image.alt = "rock"
-//   yourChoiceDiv.appendChild(image)
+// //? message
+// const messagePar = document.querySelector(".message");
 
-//   //? innerHTML
-//   // yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`
-// })
+// //? Score
+// const scoreCardSection = document.querySelector(".score-card");
+// const pcScoreSpan = document.getElementById("pc-score");
+// const yourScoreSpan = document.getElementById("your-score");
 
-// paperImg.addEventListener("click", () => {
-//   image.src = "./assets/paper.png"
-//   image.alt = "paper"
-//   yourChoiceDiv.appendChild(image)
-// })
+// //? Modal
+// const modalCardSection = document.querySelector(".modal-card");
+// const finalMessagePar = document.getElementById("final-message");
+// const playAgainBtn = document.getElementById("play-again");
 
-// scissorImg.addEventListener("click", () => {
-//   image.src = "./assets/scissor.png"
-//   image.alt = "scissor"
-//   yourChoiceDiv.appendChild(image)
-// })
+// //* ------- Variables ------- */
+// let userSelectImg = document.createElement("img");
+// let pcSelectImg = document.createElement("img");
+// let pcRandom;
+// //? Colors
+// const YELLOW = "#ffc538";
+// const RED = "#fb778b";
+// const GREEN = "#5ab7ac";
+
+// //* ------- Event Listeners ------- */
+// selectionArticle.addEventListener("click", (e) => {
+//   // console.log(e.target.id)
+//   if (e.target.id) {
+//     userSelectImg.src = `./assets/${e.target.id}.png`;
+//     userSelectImg.alt = e.target.id;
+//     yourChoiceDiv.appendChild(userSelectImg);
+//     createPcSelection();
+//   }
+// });
+
+// playAgainBtn.addEventListener("click", () => {
+//   // modalCardSection.classList.toggle("show")
+//   // modalCardSection.classList.toggle("remove")
+//   modalCardSection.style.display = "none";
+//   window.location.reload();
+// });
+
+// //* ------- Functions ------- */
+
+// const createPcSelection = () => {
+//   const pcArr = ["rock", "paper", "scissor"];
+//   pcRandom = pcArr[Math.floor(Math.random() * 3)];
+//   pcSelectImg.src = `./assets/${pcRandom}.png`;
+//   pcSelectImg.alt = pcRandom;
+//   pcChoiceDiv.appendChild(pcSelectImg);
+//   calculateResult();
+// };
+
+// const calculateResult = () => {
+//   // console.log(userSelectImg.alt)
+//   // console.log(pcSelectImg.alt)
+
+//   //? Esitlik durumu
+//   if (userSelectImg.alt === pcRandom) {
+//     draw();
+//   } else {
+//     if (userSelectImg.alt === "rock") {
+//       pcRandom === "paper" ? youLost() : youWin();
+//     } else if (userSelectImg.alt === "scissor") {
+//       pcRandom === "rock" ? youLost() : youWin();
+//     } else if (userSelectImg.alt === "paper") {
+//       pcRandom === "scissor" ? youLost() : youWin();
+//     }
+//   }
+
+//   if (pcScoreSpan.textContent === "10" || yourScoreSpan.textContent === "10") {
+//     openModal();
+//   }
+// };
+
+// const draw = () => {
+//   messagePar.textContent = "Its a draw";
+//   scoreCardSection.style.color = YELLOW;
+//   messagePar.style.backgroundColor = YELLOW;
+// };
+
+// const youLost = () => {
+//   messagePar.textContent = "You Lost";
+//   scoreCardSection.style.color = RED;
+//   messagePar.style.backgroundColor = RED;
+//   pcScoreSpan.textContent++;
+// };
+
+// const youWin = () => {
+//   messagePar.textContent = "You Win";
+//   scoreCardSection.style.color = GREEN;
+//   messagePar.style.backgroundColor = GREEN;
+//   yourScoreSpan.textContent++;
+// };
+
+// //? modal aç
+// const openModal = () => {
+//   modalCardSection.classList.add("show");
+
+//   if (yourScoreSpan.textContent === "10") {
+//     //? eger kullanici 10 puana usalti ise kullanici kazanmistir.
+//     finalMessagePar.textContent = "💃 You Win🕺";
+//     document.querySelector(".modal").style.backgroundColor = GREEN;
+//     playAgainBtn.style.color = GREEN;
+//   } else {
+//     //? eger pc 10 puana ulasti ise pc kazanmistir.
+//     finalMessagePar.textContent = "☹️ You Lost ☹️";
+//     document.querySelector(".modal").style.backgroundColor = RED;
+//     playAgainBtn.style.color = RED;
+//   }
+// };
+
+// //! Local Storage'a veri yazma ve okuma
+// localStorage.setItem("highScore", 5); //? veri yazma
+// console.log(localStorage.getItem("highScore")); //? veri okuma
+
+// //! İlkel Yontem
+// //? Resimler
+// // const rockImg = document.getElementById("rock")
+// // const paperImg = document.getElementById("paper")
+// // const scissorImg = document.getElementById("scissor")
+
+// // rockImg.addEventListener("click", () => {
+// //   image.src = "./assets/rock.png"
+// //   image.alt = "rock"
+// //   yourChoiceDiv.appendChild(image)
+
+// //   //? innerHTML
+// //   // yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`
+// // })
+
+// // paperImg.addEventListener("click", () => {
+// //   image.src = "./assets/paper.png"
+// //   image.alt = "paper"
+// //   yourChoiceDiv.appendChild(image)
+// // })
+
+// // scissorImg.addEventListener("click", () => {
+// //   image.src = "./assets/scissor.png"
+// //   image.alt = "scissor"
+// //   yourChoiceDiv.appendChild(image)
+// // })
