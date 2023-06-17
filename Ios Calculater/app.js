a4c0a7c457e791f8c153c4b00b2e4a4140f72506
Ali <alidrl26@gmail.com>
2023-06-17 15:13:11 +0200

    new version of functions added

--- Ios Calculater/app.js
+++ Ios Calculater/app.js
@@ -1,79 +1,83 @@
+/* SELECTORS */
 const buttons = document.querySelector(".buttons");
-const topScreen = document.querySelector(".upScreen");
 const bottomScreen = document.querySelector(".bottomScreen");
-const dote = document.querySelector(".dote")
+const upScreen = document.querySelector(".upScreen");
+const allScreen = document.querySelector(".screen");
+const dote = document.querySelector(".dote");
 
 /* VARIABLES */
 let fNum = 0;
 let sNum = 0;
-let result = 0;
 let choosedOp = "";
-let sayac = 0
+let result = 0;
 
-/* addEventListenerS */
+/* addEventListener */
 buttons.addEventListener("click", (e) => {
   addToBottom(e);
   operator(e);
-  dotted(e);
-  console.log();
+});
+
+bottomScreen.addEventListener("click", () => {
+  bottomScreen.innerText = bottomScreen.innerText.slice(0, -1);
 });
 
 /* FUNCTIONS */
 const addToBottom = (e) => {
+  // if (!e.target.classList.contains("buttons")) {
+  //   bottomScreen.innerText += e.target.innerText;
+  // }
+
   if (!e.target.classList.contains("buttons")) {
-    bottomScreen.innerText += e.target.innerText;
+    const buttonText = e.target.innerText;
+    if (buttonText === "." && bottomScreen.innerText.includes(".")) {
+      // Eğer eklenecek karakter nokta ise ve ekranda zaten nokta varsa işlem yapma
+      return;
+    }
+    bottomScreen.innerText += buttonText;
   }
 };
 
 const operator = (e) => {
   if (e.target.classList.contains("operator")) {
-    topScreen.innerText = bottomScreen.innerText;
-    choosedOp = topScreen.innerText[topScreen.innerText.length - 1];
-    fNum = bottomScreen.innerText.slice(0, -1);
+    upScreen.innerText = bottomScreen.innerText;
+    fNum = Number(bottomScreen.innerText.slice(0, -1));
+    choosedOp = upScreen.innerText[upScreen.innerText.length - 1];
     bottomScreen.innerText = "";
-    if (e.target.classList.contains("minus")) addMinus();
-  } else if (e.target.classList.contains("equal")) equal();
-  else if (e.target.classList.contains("clear")) clear();
+    if (e.target.classList.contains("minus")) {
+      const minus = "-";
+      bottomScreen.innerText = minus + fNum;
+      upScreen.innerText = bottomScreen.innerText;
+    }
+  } else if (e.target.classList.contains("equal")) {
+    upScreen.innerText += bottomScreen.innerText.slice(0, -1);
+    sNum = Number(bottomScreen.innerText.slice(0, -1));
+    bottomScreen.innerText = calculate(fNum, sNum, choosedOp);
+    if (
+      bottomScreen.innerText === "undefined" &&
+      e.target.classList.contains("equal")
+    ) {
+      alert("deger gir");
+      bottomScreen.innerText = "";
+    }
+  } else if (e.target.classList.contains("clear")) {
+    fNum = 0;
+    sNum = 0;
+    choosedOp = "";
+    result = 0;
+    allScreen.innerText = "";
+    location.reload();
+  }
 };
 
 const calculate = (fNum, sNum, choosedOp) => {
-  if (choosedOp === "x") return fNum * sNum;
-  else if (choosedOp === "÷") return fNum / sNum;
-  else if (choosedOp === "%") {
-    return (fNum * sNum) / 100;
-  } else if (choosedOp === "-") return fNum - sNum;
-  else if (choosedOp === "+") return Number(fNum) + Number(sNum);
-};
-
-const equal = () => {
-  topScreen.innerText += bottomScreen.innerText.slice(0, -1);
-  sNum = bottomScreen.innerText.slice(0, -1);
-  bottomScreen.innerText = calculate(fNum, sNum, choosedOp);
-};
-
-const clear = () => {
-  /*   
-  fNum = 0;
-  sNum = 0;
-  result = 0;
-  choosedOp = ""; */
-  topScreen.innerText = "";
-  bottomScreen.innerText = "";
-  location.reload();
-};
-const addMinus = () => {
-  const minus = "-";
-  bottomScreen.innerText += minus + fNum;
-  topScreen.innerText = bottomScreen.innerText;
-};
-
-const dotted = (e) => {
-  if (e.target.classList.contains("dote")) {
-    sayac++;
-    if (sayac > 1) {
-      dote.style.pointerEvents = "none";
-      //   bottomScreen.innerText = "";
-      //   alert("2 tane nokta koyamazsin");
+  if (choosedOp === "+") return fNum + sNum;
+  else if (choosedOp === "-") return (fNum - sNum).toFixed(2);
+  else if (choosedOp === "÷") {
+    if (sNum === 0) {
+      return "HATA!!!";
+    } else {
+      return (fNum / sNum).toFixed(5);
     }
-  }
+  } else if (choosedOp === "x") return (fNum * sNum).toFixed(2);
+  else if (choosedOp === "%") return (fNum * sNum) / 100;
 };
